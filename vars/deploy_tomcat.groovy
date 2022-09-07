bf9d1aae5e7fcd228aafcc908a8f9e37c7ff325e
JyotiRanSwain <108118836+JyotiRanSwain@users.noreply.github.com>
2022-09-07 21:16:17 +0530

    Update deploy_tomcat.groovy

--- vars/deploy_tomcat.groovy
+++ vars/deploy_tomcat.groovy
@@ -1,5 +1,5 @@
 def call () {
 sshagent(['8bc97ec4-b421-492e-83a1-be266d890f7c']) {
- sh "scp -o StrictHostKeyChecking=no **/*.war ec2-user@3.111.198.203:/opt/tomcat/webapps/"
+ sh "scp -o StrictHostKeyChecking=no **/*.war ec2-user@${params.staging_server}:/opt/tomcat/webapps/"
 } 
 }
